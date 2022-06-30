81743ed24b6d1d82d6838152f533bb4080bbefa3
Roberto Tyley <roberto.tyley@guardian.co.uk>
2022-06-30 21:32:47 +0100

    a bit of wavy wavy

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -1,7 +1,16 @@
+import math
+import time
+
 from tardis import windows
 
 print("HIHI")
 windows.sweep()
+
+
+windows.aw.set_constant_current(1, 255)
+for lamp_angle in range(1000):
+    windows.set_windows([int(255 * math.pow((1 + math.cos((2 * math.pi * x/8) - (lamp_angle / 10)))/2, 2)) for x in range(8)])
+    time.sleep(0.01)
 
 from tardis import tardis_keypad
 
--- circuitpython/tardis/windows.py
+++ circuitpython/tardis/windows.py
@@ -23,12 +23,17 @@ windowPinNumbers = [1, 4, 2, 5, 3, 6, 7, 12]
 # 7: 4_Black
 # 12: 4_White
 
+NUM_WINDOWS = 8
+
 def sweep():
-    for windowPinNumber in windowPinNumbers:
-        aw.set_constant_current(windowPinNumber, 224)
-        print(f"lit {windowPinNumber}")
-        time.sleep(0.1)
-        aw.set_constant_current(windowPinNumber, 0)
+    half_num_windows = int(NUM_WINDOWS / 2)
+    for n in range(3):
+        for windowIndex in range(half_num_windows):
+            aw.set_constant_current(windowPinNumbers[windowIndex], 224)
+            aw.set_constant_current(windowPinNumbers[windowIndex+half_num_windows], 224)
+            time.sleep(0.1)
+            aw.set_constant_current(windowPinNumbers[windowIndex], 0)
+            aw.set_constant_current(windowPinNumbers[windowIndex + half_num_windows], 0)
 
 
 def set_all_windows(value):
@@ -41,5 +46,20 @@ def set_all_windows(value):
     __led_buffer[6] = value  # int(value/6)
     __led_buffer[7] = value  # int(value/7)
     __led_buffer[8] = value  # int(value/8)
+    with aw.i2c_device as i2cDev:
+        i2cDev.write(__led_buffer)
+
+
+def set_windows(values):
+    __led_buffer[0] = 0x25  # Address of the register for the 1st pin (P0_1 LED current control)
+
+    __led_buffer[1] = values[0]
+    __led_buffer[2] = values[2]  # int(value/3)
+    __led_buffer[3] = values[4]  # int(values[]/5)
+    __led_buffer[4] = values[1]  # int(values[]/2)
+    __led_buffer[5] = values[3]  # int(values[]/4)
+    __led_buffer[6] = values[5]  # int(values[]/6)
+    __led_buffer[7] = values[6]  # int(values[]/7)
+    __led_buffer[8] = values[7]  # int(values[]/8)
     with aw.i2c_device as i2cDev:
         i2cDev.write(__led_buffer)
